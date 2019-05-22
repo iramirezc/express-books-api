b93cc15b4b62fcb4ee7916590eb078a3b6eaf86d
Isaac Ramirez <iramirezc@live.com.mx>
2019-05-22 02:28:10 -0500

    add env vars when running container

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@ docker container stop books-api
 printf "Removing books-api container... "
 docker container rm books-api
 printf "Running books-api container... "
-docker run -p 8080:8080 -d --name=books-api iramirezc/books-api-express
+docker run -p 8080:8080 -d --name=books-api -e NODE_ENV=development -e SERVER_HOST=0.0.0.0 -e SERVER_PORT=8080 iramirezc/books-api-express
 # wait 1 second
 sleep 1
 printf "\nTesting server app...\n"
