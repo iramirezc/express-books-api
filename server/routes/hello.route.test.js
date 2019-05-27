478325ef9145a7c6dfe76cb35b38127fca9bc96e
Isaac Ramirez <iramirezc@live.com.mx>
2019-05-27 03:16:50 -0500

    * Rename tests folder to test * Improve hello route test file

--- server/routes/hello.route.test.js
+++ server/routes/hello.route.test.js
@@ -5,18 +5,22 @@ const sinon = require('sinon')
 const HelloRoute = require('./hello.route')
 
 describe('Hello Route - Unit Tests', () => {
+  afterEach(() => {
+    sinon.restore()
+  })
+
   it('should return Hello World #sanity', () => {
     const req = httpMocks.createRequest({
       method: 'GET',
       url: '/'
     })
     const res = httpMocks.createResponse()
-    const spySend = sinon.spy(res, 'send')
+    sinon.spy(res, 'send')
 
     HelloRoute.handler(req, res)
 
     expect(res.statusCode).to.equal(200)
-    expect(spySend.calledOnceWith(sinon.match('Hello World!'))).to.equal(true)
-    expect(res._isEndCalled()).to.equal(true)
+    expect(res.finished).to.equal(true)
+    sinon.assert.calledWith(res.send, sinon.match('Hello World'))
   })
 })
